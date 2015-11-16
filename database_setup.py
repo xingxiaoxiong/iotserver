b8b5131045dfb4fd51b902a4f3c0f97829296859
Xiaoxiong Xing <xingxiaoxiong@frontier.hokudai.ac.jp>
2015-11-16 09:50:57 -0500

    add cascade mysql deletion

--- database_setup.py
+++ database_setup.py
@@ -16,25 +16,38 @@ class Farm(Base):
     api_key = Column(String(16), unique=True)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
+    greenhouse = relationship("Greenhouse",
+                    cascade="all, delete-orphan",
+                    passive_deletes=True)
+    
     
 class Greenhouse(Base):
     __tablename__ = 'greenhouse'
     
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(250), nullable=False)
-    farm_id = Column(Integer, ForeignKey('farm.id'))
-    farm = relationship(Farm)
-    
+    farm_id = Column(Integer, ForeignKey('farm.id', ondelete='CASCADE'))
+    watering = relationship("Watering",
+                    cascade="all, delete-orphan",
+                    passive_deletes=True)
+    node = relationship("Node",
+                    cascade="all, delete-orphan",
+                    passive_deletes=True)
+                    
 class Node(Base):
     __tablename__ = 'node'
     
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(250), nullable=False)
-    greenhouse_id = Column(Integer, ForeignKey('greenhouse.id'))
-    greenhouse = relationship(Greenhouse)
-    farm_id = Column(Integer, ForeignKey('farm.id'))
-    farm = relationship(Farm)
-    
+    greenhouse_id = Column(Integer, ForeignKey('greenhouse.id', ondelete='CASCADE'))
+    farm_id = Column(Integer)
+    temperature = relationship("Temperature",
+                    cascade="all, delete-orphan",
+                    passive_deletes=True)
+    humidity = relationship("Temperature",
+                    cascade="all, delete-orphan",
+                    passive_deletes=True)
+
     
 class Temperature(Base):
     __tablename__ = 'temperature'
@@ -42,8 +55,7 @@ class Temperature(Base):
     id = Column(Integer, primary_key=True)
     value = Column(Float)
     datetime = Column(DateTime, default=datetime.datetime.utcnow)
-    node_id = Column(Integer, ForeignKey('node.id'))
-    node = relationship(Node)
+    node_id = Column(Integer, ForeignKey('node.id', ondelete='CASCADE'))
 
 class Humidity(Base):
     __tablename__ = 'humidity'
@@ -51,17 +63,15 @@ class Humidity(Base):
     id = Column(Integer, primary_key=True)
     value = Column(Float)
     datetime = Column(DateTime, default=datetime.datetime.utcnow)
-    node_id = Column(Integer, ForeignKey('node.id'))
-    node = relationship(Node)
-    
+    node_id = Column(Integer, ForeignKey('node.id', ondelete='CASCADE'))
+
 class Watering(Base):
     __tablename__ = 'watering'
 
     id = Column(Integer, primary_key=True)
     time = Column(Integer)
     date = Column(Date)
-    greenhouse_id = Column(Integer, ForeignKey('greenhouse.id'))
-    greenhouse = relationship(Greenhouse)
+    greenhouse_id = Column(Integer, ForeignKey('greenhouse.id', ondelete='CASCADE'))
 
 
 if __name__ == '__main__':
